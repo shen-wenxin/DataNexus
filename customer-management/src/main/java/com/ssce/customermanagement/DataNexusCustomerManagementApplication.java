2a250fbe5f074ff9a337c731e33f9d6ee41ba9c6
wenxinShen <13326874+wenxinshen@user.noreply.gitee.com>
2023-08-29 09:26:49 +0800

    first init SpringBoot Application

---
+++ customer-management/src/main/java/com/ssce/customermanagement/DataNexusCustomerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ssce.customermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataNexusCustomerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataNexusCustomerManagementApplication.class, args);
+	}
+
+}
---
+++ customer-management/src/test/java/com/ssce/customermanagement/DataNexusCustomerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssce.customermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataNexusCustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
