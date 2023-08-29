acddcbda2717fc149783f3149d7c7b4639817d85
wenxinShen <13326874+wenxinshen@user.noreply.gitee.com>
2023-08-29 22:25:21 +0800

    修复了版本不兼容导致mvn不能用的问题

--- customer-management/src/main/java/com/szsc/customermanagement/CustomerManagementApplication.java
+++ customer-management/src/main/java/com/szsc/customermanagement/CustomerManagementApplication.java
@@ -1,13 +1,13 @@
-package com.ssce.customermanagement;
+package com.szsc.customermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataNexusCustomerManagementApplication {
+public class CustomerManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataNexusCustomerManagementApplication.class, args);
+		SpringApplication.run(CustomerManagementApplication.class, args);
 	}
 
 }
--- customer-management/src/test/java/com/szsc/customermanagement/CustomerManagementApplicationTests.java
+++ customer-management/src/test/java/com/szsc/customermanagement/CustomerManagementApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ssce.customermanagement;
+package com.szsc.customermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DataNexusCustomerManagementApplicationTests {
+class CustomerManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
