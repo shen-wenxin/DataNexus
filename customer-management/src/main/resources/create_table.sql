fbddf4153c4045c82fd40037bb04e20cc8d7370f
wenxinShen <13326874+wenxinshen@user.noreply.gitee.com>
2023-09-05 15:58:19 +0800

    修改了初始化的sql表创建方式

--- customer-management/src/main/resources/create_table.sql
+++ customer-management/src/main/resources/create_table.sql
@@ -1,62 +1,37 @@
-CREATE TABLE IF NOT EXISTS data_nexus_db.company_main_information (
-    company_id INT PRIMARY KEY,
-    company_code VARCHAR(50) NOT NULL,
-    company_name VARCHAR(100) NOT NULL,
-    company_abbreviation VARCHAR(50),
-    company_type VARCHAR(50),
-    INDEX idx_company_code (company_code),
-    INDEX idx_company_name (company_name)
-);
-
-CREATE TABLE IF NOT EXISTS data_nexus_db.company_detailed_information (
-    company_id INT PRIMARY KEY,
-    unified_social_credit VARCHAR(50),
-    registered_address VARCHAR(200),
-    registered_phone VARCHAR(20),
-    company_email VARCHAR(100),
-    establishment_date DATE,
-    registered_capital DECIMAL(18,2),
-    legal_representative_name VARCHAR(100),
-    legal_representative_phone VARCHAR(20),
-    legal_representative_id VARCHAR(50),
-    industry VARCHAR(50),
-    business_scope VARCHAR(200),
-    is_verified_customer BOOLEAN,
-    customer_status VARCHAR(50),
-    country VARCHAR(50),
-    province VARCHAR(50),
-    city VARCHAR(50),
-    business_license_number VARCHAR(50),
-    business_license_expiry DATE,
-    primary_contact_name VARCHAR(100),
-    primary_contact_position VARCHAR(50),
-    primary_contact_phone VARCHAR(20),
-    primary_contact_email VARCHAR(100),
-    FOREIGN KEY (company_id) REFERENCES data_nexus_db.company_main_information (company_id),
-    INDEX idx_unified_social_credit (unified_social_credit)
-);
-
-CREATE TABLE IF NOT EXISTS data_nexus_db.registered_location (
+CREATE TABLE IF NOT EXISTS data_nexus_db.company_information (
   company_id INT PRIMARY KEY,
-  location ENUM('chinaMainland', 'HongKong', 'overseas'),
-  FOREIGN KEY (company_id) REFERENCES data_nexus_db.company_main_information (company_id),
-  INDEX (company_id)
-);
-
-CREATE TABLE IF NOT EXISTS data_nexus_db.scope_information (
-  company_id INT,
-  scope VARCHAR(200),
-  PRIMARY KEY (company_id),
-  FOREIGN KEY (company_id) REFERENCES data_nexus_db.company_main_information (company_id),
-  INDEX (company_id)
-);
-
-CREATE TABLE IF NOT EXISTS szse_membership (
-  szse_member_id INT PRIMARY KEY,
-  company_id INT,
+  company_code VARCHAR(50) NOT NULL,
+  company_name VARCHAR(100) NOT NULL,
+  company_abbreviation VARCHAR(50),
+  registered_location ENUM('Mainland', 'HongKong', 'Overseas'),
+  company_type VARCHAR(50),
+  unified_social_credit VARCHAR(50),
+  registered_address VARCHAR(200),
+  registered_phone VARCHAR(20),
+  company_email VARCHAR(100),
+  establishment_date DATE,
+  registered_capital DECIMAL(18, 2),
+  legal_representative_name VARCHAR(100),
+  legal_representative_phone VARCHAR(20),
+  legal_representative_id VARCHAR(50),
+  industry VARCHAR(50),
+  business_scope VARCHAR(200),
+  is_verified_customer BOOLEAN,
   is_szse_member BOOLEAN,
   szse_member_code VARCHAR(50),
   szse_member_abbreviation VARCHAR(50),
-  FOREIGN KEY (company_id) REFERENCES data_nexus_db.company_main_information (company_id),
-  INDEX (is_szse_member)
+  customer_status VARCHAR(50),
+  country VARCHAR(50),
+  province VARCHAR(50),
+  city VARCHAR(50),
+  business_license_number VARCHAR(50),
+  business_license_expiry DATE,
+  primary_contact_name VARCHAR(100),
+  primary_contact_position VARCHAR(50),
+  primary_contact_phone VARCHAR(20),
+  primary_contact_email VARCHAR(100),
+  INDEX idx_company_id (company_id),
+  INDEX idx_company_code (company_code),
+  INDEX idx_company_name (company_name),
+  INDEX idx_unified_social_credit (unified_social_credit)
 );
