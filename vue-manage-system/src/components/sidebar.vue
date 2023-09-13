22df00192ba6a9713c76605bd0c4198dcf0434eb
wenxinShen <13326874+wenxinshen@user.noreply.gitee.com>
2023-09-13 16:52:02 +0800

    精简了前端

--- vue-manage-system/src/components/sidebar.vue
+++ vue-manage-system/src/components/sidebar.vue
@@ -65,19 +65,14 @@ const items = [
 
 
     {
-        icon: 'Calendar',
+        icon: 'DocumentCopy',
         index: '1',
         title: '表格相关',
         permiss: '2',
         subs: [
             {
-                index: '/table',
-                title: '常用表格',
-                permiss: '2',
-            },
-            {
                 index: '/companies',
-                title: '公司信息',
+                title: '管理公司信息',
                 permiss: '2',
             },
             {
@@ -85,83 +80,14 @@ const items = [
                 title: '查看历史纪录',
                 permiss: '1'
             },
-            {
-                index: '/export',
-                title: '导出Excel',
-                permiss: '2',
-            },
         ],
     },
-    {
-        icon: 'DocumentCopy',
-        index: '/tabs',
-        title: 'tab选项卡',
-        permiss: '3',
-    },
+    
     {
         icon: 'Edit',
-        index: '3',
-        title: '管理公司信息',
+        index: '/CompanyAdd',
+        title: '新增公司',
         permiss: '4',
-        subs: [
-            {
-                index: '/form',
-                title: '基本表单',
-                permiss: '5',
-            },
-            {
-                index:'/CompanyAdd',
-                title: '新增公司',
-                permiss: '5',
-
-            },
-            {
-                index: '/upload',
-                title: '文件上传',
-                permiss: '6',
-            },
-            {
-                index: '4',
-                title: '三级菜单',
-                permiss: '7',
-                subs: [
-                    {
-                        index: '/editor',
-                        title: '富文本编辑器',
-                        permiss: '8',
-                    },
-                    {
-                        index: '/markdown',
-                        title: 'markdown编辑器',
-                        permiss: '9',
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        icon: 'Setting',
-        index: '/icon',
-        title: '自定义图标',
-        permiss: '10',
-    },
-    {
-        icon: 'PieChart',
-        index: '/charts',
-        title: 'schart图表',
-        permiss: '11',
-    },
-    {
-        icon: 'Warning',
-        index: '/permission',
-        title: '权限管理',
-        permiss: '13',
-    },
-    {
-        icon: 'CoffeeCup',
-        index: '/donate',
-        title: '支持作者',
-        permiss: '14',
     },
 ];
 
